05e447efc303fb327f53145e9690f17ad854c33a
kamiyo <kamiyo@gmail.com>
2016-11-28 02:09:15 -0600

    finished tests I think

--- Source/UnitTests/RingBufferTest.cpp
+++ Source/UnitTests/RingBufferTest.cpp
@@ -27,68 +27,82 @@ TEST(RingBuffer, RingBuffer)
   EXPECT_EQ(0u, rb.LoadTail());
 
   // check random access and circularity
-  rb[5] = 10;
-  EXPECT_EQ(10u, rb[5]);
-  EXPECT_EQ(rb[5], rb[15]);
-  rb[25] = 1;
-  EXPECT_EQ(1u, rb[5]);
+  // writes: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  // and then writes: [0, 11, 22, 33, 44, 55, 66, 77, 88, 99, 110]
+  for (size_t i = 0; i < rb.MaxSize(); i++) {
+	  rb[i] = i;
+	  EXPECT_EQ(static_cast<u32>(i), rb[i]);
+	  EXPECT_EQ(static_cast<u32>(i), rb[i + rb.MaxSize() * i]);
+	  rb[i + rb.MaxSize() * i] = i + rb.MaxSize() * i;
+	  EXPECT_EQ(static_cast<u32>(i + rb.MaxSize() * i), rb[i]);
+  }
+  // should not affect head and tail
+  EXPECT_EQ(0u, rb.LoadHead());
+  EXPECT_EQ(0u, rb.LoadTail());
 
+  // 0s the array
+  // we don't need to reset head and tail because they haven't been moved
   std::fill_n(&rb[0], rb.MaxSize(), 0);
 
   std::array<u32, 20> ones{ 1 };
   std::array<u32, 20> twos{ 2 };
+  std::array<u32, 20> threes{ 3 };
 
   // write 5 items, check
+  // should be: [1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
+  //             ^tail          ^ head
   rb.Write(ones.data(), 5);
   EXPECT_EQ(1u, rb[0]);
   EXPECT_EQ(1u, rb[4]);
+  EXPECT_EQ(0u, rb[5]);
   EXPECT_EQ(5u, rb.LoadHead());
   EXPECT_EQ(0u, rb.LoadTail());
 
   // write 10 items, but since tail hasn't moved from 0,
   // should only write 5 items (current head is at 5)
+  // should be : [1, 1, 1, 1, 1, 2, 2, 2, 2, 2]
+  //              ^tail,head
   rb.Write(twos.data(), 10);
   EXPECT_EQ(10u, rb.LoadHead());
   EXPECT_EQ(2u, rb[rb.LoadHead() - 1]);
   EXPECT_EQ(1u, rb[rb.LoadHead()]);
 
-  // move tail up
+  // move tail up, test fetchadd and store
   rb.FetchAddTail(5);
   EXPECT_EQ(5u, rb.LoadTail());
+  // now: [1, 1, 1, 1, 1, 2, 2, 2, 2, 2]
+  //       ^head          ^tail
   rb.StoreTail(10);
   EXPECT_EQ(10u, rb.LoadTail());
+  // now: [1, 1, 1, 1, 1, 2, 2, 2, 2, 2]
+  //       ^head,tail
 
   // since tail == head, fetchadd should not change anything
+  // because tail cannot be greater than head
   rb.FetchAddTail(25);
   EXPECT_EQ(rb.LoadHead(), rb.LoadTail());
 
-  // move head down; should do nothing
+  // move head down; should do nothing here
   rb.StoreHead(0);
   EXPECT_EQ(10u, rb.LoadHead());
-  rb.StoreHead(15);
 
-  // START CODING HERE
+  // should work here, causing:
+  // [1, 1, 1, 1, 1, 2, 2, 2, 2, 2]
+  //  ^tail                ^head
+  rb.StoreHead(17);
 
-  // check circle-write past tail
-  // should do nothing
-  rb1.Write(ones.data(), ones.size());
-
-  EXPECT_EQ(50000u, rb1.LoadHead());
-  EXPECT_EQ(0u, rb1[0]);
-
-  // move tail, then check circle-write
-  rb1.FetchAddTail(1);
-  EXPECT_EQ(1u, rb1.LoadTail());
-  rb1.Write(ones.data(), ones.size());
-
-  EXPECT_EQ(100001u, rb1.LoadHead());
-  EXPECT_EQ(1u, rb1[0]);
-
-  rb1[0] = 9;
-  // check circle-ness
-  EXPECT_EQ(9u, rb1[rb1.MaxSize()]);
-  EXPECT_EQ(9u, rb1[rb1.MaxSize() * 2]);
-
-  rb1[rb1.MaxSize()] = 5;
-  EXPECT_EQ(5u, rb1[0]);
+  // circle write
+  // start head  at 17 and tail at 15
+  // writing 6 elements should wrap around 3 element
+  // head should be at 23
+  rb.StoreTail(15);
+  rb.Write(threes.data(), 6);
+  EXPECT_EQ(3u, rb[0]);
+  EXPECT_EQ(3u, rb[2]);
+  EXPECT_EQ(3u, rb[7]);
+  EXPECT_EQ(23u, rb.LoadHead());
+  EXPECT_EQ(1u, rb[rb.LoadHead()]);
+  EXPECT_EQ(2u, rb[rb.LoadTail()]);
+  // [3, 3, 3, 1, 1, 2, 2, 3, 3, 3]
+  //           ^head ^tail
 }
